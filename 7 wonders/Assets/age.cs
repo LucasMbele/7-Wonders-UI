de05a457378dc3b844c1ed852a4de3a7f6b17904
Luc Prucell <lucprucell@gmail.com>
2020-11-29 05:01:31 +0300

    Beta-version

---
+++ 7 wonders/Assets/age.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class age : MonoBehaviour
+{
+    public Sprite[] age_game = new Sprite[]{} ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When the first round will be over, then we'll change the age number and arrow direction
+        System.Random r = new System.Random();
+        int rInts = r.Next(0,2);
+
+        this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = age_game[rInts];
+        
+    }
+
+    // Update is called once per frame
+  
+}
---
+++ 7 wonders/Assets/background_game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_game : MonoBehaviour
+{
+    public Sprite[] background = new Sprite[]{} ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random r = new System.Random();
+        int rInts = r.Next(0,8);
+
+        this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = background[rInts];
+        
+    }
+
+    // Update is called once per frame
+  
+}
